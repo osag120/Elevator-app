7d713b329ba135b105bd64c77b32a2d449caf10a
osag120 <isyjes8@gmail.com>
2024-08-27 12:57:11 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let sam = alert('let get you started')
+prompt('Enter your Name')
+prompt('input your Age')
+prompt('Do you really want this job')
+alert('you are doing great')
+prompt('enter your state of origin')
+prompt('input your email address')
+prompt('Input your Password')
+prompt('phone number(11 numbers)')
+alert('welldone we have received your information, continue on our website')
+console.log(sam);
+
